0ee6e16b6aa15c1bc76a7c343faae7652388c84b
Meredith Espinosa <6722691+LemmaEOF@users.noreply.github.com>
2025-03-13 05:29:16 -0700

    yknow what I better get at least something onto mr in time

--- src/main/java/gay/lemmaeof/barkeep/block/CocktailGlassBlock.java
+++ src/main/java/gay/lemmaeof/barkeep/block/CocktailGlassBlock.java
@@ -21,16 +21,16 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-//TODO: multiple types of glass
 public class CocktailGlassBlock extends Block implements BlockEntityProvider {
 	//TODO: BER instead of block model? would let me do fancy animations and variable drink sizes, and we need one anyway for garniture
 	//TODO: oh if it's a BER I should make shaken drinks more opaque bc they're aerated
 	public static final BooleanProperty FILLED = BooleanProperty.of("filled");
-	public static final VoxelShape SHAPE = Block.createCuboidShape(5, 0, 5, 11, 10, 11);
+	//TODO: change based on the glass, probably just pass to ctor
+	public static final VoxelShape SHAPE = Block.createCuboidShape(5, 0, 5, 11, 8, 11);
 
 	private final int capacity;
 
-	//TODO: cup capacity?
+	//TODO: make cup capacity real?
 	public CocktailGlassBlock(int capacity, Settings settings) {
 		super(settings);
 		this.capacity = capacity;
